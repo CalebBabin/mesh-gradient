3d6570f7df3fa47c10f181df32ff41a04b374674
calebbabin <object.caleb@gmail.com>
2024-12-31 20:55:21 -0500

    don't allow multiple active color pickers

--- src/utils/colorPicker.jsx
+++ src/utils/colorPicker.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { SketchPicker } from 'react-color';
 
 
 export function ColorPicker({ color, onChange }) {
 	const [active, setActive] = useState(false);
+	const ref = useRef();
 
-	return <button className='relative' onClick={() => setActive(!active)}>
+	useEffect(() => {
+		const listener = e => {
+			if (!ref.current) return;
+
+			if (active && !ref.current.contains(e.target)) {
+				setActive(false)
+			};
+		}
+		window.addEventListener('click', listener);
+
+		return () => window.removeEventListener('click', listener);
+	}, [ref, active]);
+
+	return <button ref={ref} className='relative' onClick={() => setActive(!active)}>
 		<div className='absolute inset-1' style={{
 			background: color ? `rgb(${color.r},${color.g},${color.b})` : '#000',
 		}} />
